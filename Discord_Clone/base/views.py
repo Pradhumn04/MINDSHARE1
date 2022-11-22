bc82ecb67619754f91ca796c5b8cdc5b789180ec
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-11-22 15:11:33 +0530

    [Feat]: getting room by id

--- Discord_Clone/base/views.py
+++ Discord_Clone/base/views.py
@@ -15,5 +15,10 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
     # return HttpResponse("Room Page")
-    return render(request, 'base/room.html')
+    return render(request, 'base/room.html', context)
