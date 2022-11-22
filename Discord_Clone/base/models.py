b4356833f1672fbc1f8dc4f917838b171c7a5986
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-11-22 20:49:07 +0530

    [Feat]: adding Room model

--- Discord_Clone/base/models.py
+++ Discord_Clone/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    # host =
+    # topic =
+    # participation =
+    # when we updated the instance
+    updated =models.DateTimeField(auto_now=True)
+    # when we created the instance
+    created = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self):
+        return self.name
